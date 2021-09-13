ca732526a111f89896d0a399db3fd97130a09056
cshepscorp <sheppard.christy@gmail.com>
2021-09-12 21:39:24 -0400

    made user inputs create new div to hold task data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,38 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDo = document.querySelector("#tasks-to-do");
 
 
 
 var createTaskHandler = function() {
+
+    // prevents browser from refreshing the page when form submits
+    event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    // give it a class name
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDo.appendChild(listItemEl);
+
+    console.dir(listItemEl);
     };
 
-buttonEl.addEventListener("click", createTaskHandler);
-// console.log(buttonEl);
+formEl.addEventListener("submit", createTaskHandler);
 
 
-
-var footerEl = document.querySelector("footer");
-footerEl.textContent = "2021 Taskinator";
+document.getElementById("year").innerHTML = new Date().getFullYear() + " Taskinator";
